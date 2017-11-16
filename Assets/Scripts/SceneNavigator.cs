5e209929e5df9c7f1f335d9c4e26d7b6025d8cdf
Pablo Benedicto <pablo.benedicto@gmail.com>
2017-11-16 22:25:14 +0100

    Added comments

--- Assets/Scripts/SceneNavigator.cs
+++ Assets/Scripts/SceneNavigator.cs
@@ -3,15 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class SceneNavigator : MonoBehaviour {
 
+    /// <summary>
+    /// Carga la escena de juego.</summary>
     public void StartGame() {
         SceneManager.LoadScene("Game");
     }
 
+    /// <summary>
+    /// Sale del juego.</summary>
     public void ExitGame() {
         Debug.Log("Saliendo del juego...");
         Application.Quit();
     }
 
+    /// <summary>
+    /// Carga la escena final.</summary>
     public void EndGame() {
         SceneManager.LoadScene("End");
     }
